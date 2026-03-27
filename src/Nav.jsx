cbe8e4d52c5d3f9a6ea73cb6876e17bfc3045750
Aryan Yadav <aryanyadav9660@gmail.com>
2026-03-27 12:52:29 +0530

    Update Nav.jsx

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -11,7 +11,7 @@ const [isMenuOpen, setIsMenuOpen] = useState(false);
                 {isMenuOpen ? '✕' : '☰'}
             </button>
             {/* Hidden on small screens, flex on medium (768px) and up */}
-            <ul className={`${isMenuOpen ? 'flex' : 'hidden'} md:flex flex-col md:flex-row absolute md:static top-16 left-0 w-full md:w-auto bg-black md:bg-transparent p-6 md:p-0 gap-6 transition-all`}>
+            <ul className={`${isMenuOpen ? 'flex' : 'hidden'} md:flex flex-col md:flex-row absolute md:static text-white top-16 left-0 w-full md:w-auto bg-black md:bg-transparent p-6 md:p-0 gap-6 transition-all`}>
                 <li ><a href="#hero" onClick={() => setIsMenuOpen(false)} >Home</a></li>
                 <li><a href="#about" onClick={() => setIsMenuOpen(false)}>About</a></li>
                 <li><a href="#skills" onClick={() => setIsMenuOpen(false)}>Skills</a></li>
