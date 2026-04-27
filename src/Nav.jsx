cc0a096b9c8cb1a589f877f6e15c8baa2c1ab498
Aryan Yadav <aryanyadav9660@gmail.com>
2026-04-27 22:27:49 +0530

    Update Nav.jsx

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -12,14 +12,14 @@ const [isMenuOpen, setIsMenuOpen] = useState(false);
             <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="md:hidden text-white text-2xl">
                 {isMenuOpen ? '✕' : '☰'}
             </button>
-            {/* Hidden on small screens, flex on medium (768px) and up */}
-            <ul className={`${isMenuOpen ? 'flex' : 'hidden'} md:flex flex-col md:flex-row absolute md:static top-16 left-0 w-full md:w-auto bg-black md:bg-transparent p-6 md:p-0 gap-6 transition-all`}>
-                <li><a href="#hero" onClick={() => setIsMenuOpen(false)} >Home</a></li>
-                <li><a href="#about" onClick={() => setIsMenuOpen(false)}>About</a></li>
-                <li><a href="#skills" onClick={() => setIsMenuOpen(false)}>Skills</a></li>
-                <li><a href="#projects" onClick={() => setIsMenuOpen(false)}>Projects</a></li>
-                <li><a href="#Certificates" onClick={() => setIsMenuOpen(false)}>Certificates</a></li>
-                <li><a href="#contact" onClick={() => setIsMenuOpen(false)}>Contact</a></li>
+           
+           <ul className={`${isMenuOpen ? 'flex' : 'hidden'} md:flex flex-col md:flex-row absolute md:static top-16 left-0 w-full md:w-auto bg-black md:bg-transparent p-6 md:p-0 gap-6 transition-all`}>
+                <li><a href="#hero" onClick={() => setIsMenuOpen(false)} className="text-sky-500 hover:text-sky-700 transition duration-300 cursor-pointer" >Home</a></li>
+                <li><a href="#about" onClick={() => setIsMenuOpen(false)} className="text-sky-500 hover:text-sky-700 transition duration-300 cursor-pointer" >About</a></li>
+                <li><a href="#skills" onClick={() => setIsMenuOpen(false)} className="text-sky-500 hover:text-sky-700 transition duration-300 cursor-pointer" >Skills</a></li>
+                <li><a href="#projects" onClick={() => setIsMenuOpen(false)} className="text-sky-500 hover:text-sky-700 transition duration-300 cursor-pointer" >Projects</a></li>
+                <li><a href="#Certificates" onClick={() => setIsMenuOpen(false)} className="text-sky-500 hover:text-sky-700 transition duration-300 cursor-pointer" >Certificates</a></li>
+                <li><a href="#contact" onClick={() => setIsMenuOpen(false)} className="text-sky-500 hover:text-sky-700 transition duration-300 cursor-pointer" >Contact</a></li>
             </ul>
 
             
